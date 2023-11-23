a8aeb6a8e6e4fb1613bebfc05bd997b533bdc159
sangchu <didi1484@gmail.com>
2023-11-23 21:15:54 +0900

    feat: 구매 금액 검증 메서드 추가

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,4 +1,29 @@
 package lotto.domain;
 
+import static lotto.global.Error.INVALID_AMOUNT_RANGE;
+import static lotto.global.Error.INVALID_UNIT;
+
 public class PurchaseAmount {
+    private static final int PURCHASE_UNIT = 1_000;
+    private static final int MAX_AMOUNT = 100_000;
+    private static final int MIN_AMOUNT = 1_000;
+    private final int purchaseAmount;
+
+    public PurchaseAmount(int purchaseAmount) {
+        validateUnit(purchaseAmount);
+        validateRange(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    private void validateUnit(int purchaseAmount) {
+        if (purchaseAmount % PURCHASE_UNIT != 0) {
+            throw new IllegalArgumentException(INVALID_UNIT.getMessage());
+        }
+    }
+
+    private void validateRange(int purchaseAmount) {
+        if (purchaseAmount < MIN_AMOUNT || purchaseAmount > MAX_AMOUNT) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_RANGE.getMessage());
+        }
+    }
 }
--- src/main/java/lotto/global/Error.java
+++ src/main/java/lotto/global/Error.java
@@ -1,7 +1,9 @@
 package lotto.global;
 
 public enum Error {
-    INVALID_TYPE("숫자만 입력 가능합니다.");
+    INVALID_TYPE("숫자만 입력 가능합니다."),
+    INVALID_UNIT("1,000원 단위로만 입력 가능합니다."),
+    INVALID_AMOUNT_RANGE("구매금액은 1,000원 이상 100,000원 이하여야 합니다.");
     private static final String ERROR = "[ERROR] ";
     private final String message;
 
