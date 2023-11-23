8f43a736aa507f06b6e33bdd33d2bb8549d218c2
sangchu <didi1484@gmail.com>
2023-11-23 19:41:52 +0900

    fix: 구매 금액 입력 메소드 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,9 +3,11 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INVALID_TYPE = "[ERROR] 숫자만 입력 가능합니다.";
 
     public int askPurchaseAmount() {
+        System.out.println(ASK_PURCHASE_AMOUNT);
         String input = Console.readLine();
         validateType(input);
         return Integer.parseInt(input);
