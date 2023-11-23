0be6d3fe6fe79c2dc90ddf1c6e3d55cf60ea3e0a
sangchu <didi1484@gmail.com>
2023-11-23 21:48:30 +0900

    feat: 구매 갯수를 받아 로또 발행 메서드 추가

--- src/main/java/lotto/domain/LottoRepository.java
+++ src/main/java/lotto/domain/LottoRepository.java
@@ -1,4 +1,15 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoRepository {
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public void issueLottos(int purchaseQuantity) {
+        for (int i = 0; i < purchaseQuantity; i++) {
+            Lotto lotto = new Lotto(NumbersGenerator.generateNumbers());
+            lottos.add(lotto);
+        }
+    }
 }
