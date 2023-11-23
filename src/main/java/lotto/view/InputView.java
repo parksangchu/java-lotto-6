57b5dd4fc2f5780a5d2d98ec8ccff4b1840390ef
sangchu <didi1484@gmail.com>
2023-11-23 19:40:24 +0900

    feat: 구매 금액 입력 메소드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,20 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String INVALID_TYPE = "[ERROR] 숫자만 입력 가능합니다.";
+
+    public int askPurchaseAmount() {
+        String input = Console.readLine();
+        validateType(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateType(String input) {
+        if (!input.chars()
+                .allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException(INVALID_TYPE);
+        }
+    }
 }
