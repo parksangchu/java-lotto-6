4b375d8407b3023b49cace9d5fdfbd83972e23d4
sangchu <didi1484@gmail.com>
2023-11-23 19:49:57 +0900

    feat: 에러 메시지 저장 enum 추가

--- src/main/java/lotto/global/Error.java
+++ src/main/java/lotto/global/Error.java
@@ -1,4 +1,15 @@
 package lotto.global;
 
 public enum Error {
+    INVALID_TYPE("숫자만 입력 가능합니다.");
+    private static final String ERROR = "[ERROR] ";
+    private final String message;
+
+    Error(String message) {
+        this.message = ERROR + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,11 @@
 package lotto.view;
 
+import static lotto.global.Error.INVALID_TYPE;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String INVALID_TYPE = "[ERROR] 숫자만 입력 가능합니다.";
 
     public int askPurchaseAmount() {
         System.out.println(ASK_PURCHASE_AMOUNT);
@@ -16,7 +17,7 @@ public class InputView {
     private void validateType(String input) {
         if (!input.chars()
                 .allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException(INVALID_TYPE);
+            throw new IllegalArgumentException(INVALID_TYPE.getMessage());
         }
     }
 }
