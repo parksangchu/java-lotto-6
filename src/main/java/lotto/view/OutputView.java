b2db76071f4b59383c3e607533c8cd9e820972f4
sangchu <didi1484@gmail.com>
2023-11-24 20:00:55 +0900

    refactor: 당첨 통계 포맷 리팩터링

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -14,12 +14,13 @@ import lotto.domain.Rank;
 public class OutputView {
     private static final String PURCHASE_QUANTITY_FORMAT = "%n%d개를 구매했습니다.%n";
     private static final String WINNING_STATS = "당첨 통계\n---";
-    private static final String WINNING_STATS_FORMAT =
-            "3개 일치 (5,000원) - %d개\n"
-                    + "4개 일치 (50,000원) - %d개\n"
-                    + "5개 일치 (1,500,000원) - %d개\n"
-                    + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"
-                    + "6개 일치 (2,000,000,000원) - %d개\n";
+    private static final String WINNING_STATS_FORMAT = """
+            3개 일치 (5,000원) - %d개
+            4개 일치 (50,000원) - %d개
+            5개 일치 (1,500,000원) - %d개
+            5개 일치, 보너스 볼 일치 (30,000,000원) - %d개
+            6개 일치 (2,000,000,000원) - %d개
+            """;
 
     public static void printError(Exception e) {
         System.out.println(e.getMessage());
