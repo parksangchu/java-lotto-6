29b777d5ad77b23e99ebe307962868ef15887c18
sangchu <didi1484@gmail.com>
2023-11-24 13:59:57 +0900

    feat: 당첨 번호 입력 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,15 +3,32 @@ package lotto.view;
 import static lotto.global.Error.INVALID_TYPE;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final String ASK_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String ASK_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
+    private static final String ASK_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+
 
     public static int askPurchaseAmount() {
         System.out.println(ASK_PURCHASE_AMOUNT);
         String input = Console.readLine();
         validateType(input);
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> askWinningNumbers() {
+        System.out.println(ASK_WINNING_NUMBERS);
+        String input = Console.readLine();
+        return Arrays.stream(input.trim()
+                        .split(","))
+                .peek(number -> validateType(number))
+                .mapToInt(number -> Integer.parseInt(number))
+                .boxed()
+                .collect(Collectors.toList());
     }
 
     private static void validateType(String input) {
