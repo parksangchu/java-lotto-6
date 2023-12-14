b4a9c275e594c9dec32093058a62d4d9fc619f00
sangchu <didi1484@gmail.com>
2023-12-14 16:38:31 +0900

    feat: 로또 번호 발생 기능 추가

--- src/main/java/lotto/domain/LottoMaker.java
+++ src/main/java/lotto/domain/LottoMaker.java
@@ -1,4 +1,15 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class LottoMaker {
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
+    private static final int NUMBERS_SIZE = 6;
+
+    public static Lotto generateLottoNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_RANGE, MAX_RANGE, NUMBERS_SIZE);
+        return new Lotto(numbers);
+    }
 }
