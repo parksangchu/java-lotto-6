c4fb436750a8d135bedff31c36f77c984e1738a0
sangchu <didi1484@gmail.com>
2023-12-14 16:26:49 +0900

    feat: 로또샵 객체 생성 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,7 +1,21 @@
 package lotto.controller;
 
+import lotto.domain.LottoShop;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Controller {
     public void start() {
+        LottoShop lottoShop = createLottoShop();
+    }
 
+    private LottoShop createLottoShop() {
+        while (true) {
+            try {
+                return new LottoShop(InputView.readPurchaseAmount());
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e);
+            }
+        }
     }
 }
