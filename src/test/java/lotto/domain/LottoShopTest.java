258d1f46b4305b259e6e419fb63433eaad3f130b
sangchu <didi1484@gmail.com>
2023-12-14 16:23:27 +0900

    test: 로또 구메 금액 검증 기능 추가

---
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class LottoShopTest {
+    @Test
+    void create() {
+        assertThatThrownBy(() -> new LottoShop(900))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+        assertThatThrownBy(() -> new LottoShop(1001))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}
